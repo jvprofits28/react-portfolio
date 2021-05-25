391bd5352c8c6cf750452d309a7bf0fb392a8d6b
Juan Vazquez <ehulises28@hotmail.com>
2021-05-24 22:56:13 -0700

    did about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,31 @@
 import React from "react";
+function About() {
+  return (
+    <section className="title">
+      <h2 class="name"> Juan Vazquez</h2>
+      <hr></hr>
+
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+          <p>coming soon...</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            mollis aliquam ut porttitor leo a diam sollicitudin. Enim sit amet
+            venenatis urna cursus eget nunc scelerisque. Adipiscing vitae proin
+            sagittis nisl. Pellentesque elit ullamcorper dignissim cras
+            tincidunt lobortis feugiat vivamus. Dictum varius duis at
+            consectetur lorem donec massa sapien faucibus. Enim neque volutpat
+            ac tincidunt vitae. Fringilla est ullamcorper eget nulla. Sit amet
+            aliquam id diam maecenas ultricies mi eget. Est sit amet facilisis
+            magna etiam tempor orci eu lobortis. Porttitor leo a diam
+            sollicitudin tempor id eu nisl. Viverra justo nec ultrices dui
+            sapien.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
