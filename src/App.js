fd513161332653c481def734e755905457d6c9cf
Juan Vazquez <ehulises28@hotmail.com>
2021-05-24 22:37:39 -0700

    imported components and simpl app function

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
 import React from "react";
+import Header from "./components/Header/index";
+import NavBar from "./components/NavPage/index";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
